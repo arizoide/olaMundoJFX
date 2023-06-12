67a35be86535ff9eef6d12ed085009be9e87326f
arizoide <ari.thomazini@gmail.com>
2023-06-12 20:26:03 -0300

    Versionamento inicial do projeto para a turma de Ti 2022 Noturno do Senac

---
+++ src/main/java/sp/senac/br/olamundo/HelloController.java
@@ -0,0 +1,33 @@
+package sp.senac.br.olamundo;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+
+    public void onLimparButtonClick() {
+        welcomeText.setText("");
+    }
+
+    public void abrirTelaHello(ActionEvent actionEvent) throws IOException {
+        var fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        Stage stage = new Stage();
+        stage.setTitle("Hello!");
+        stage.setScene(scene);
+        stage.show();
+    }
+}
---
+++ src/main/java/sp/senac/br/olamundo/TesteController.java
@@ -0,0 +1,31 @@
+package sp.senac.br.olamundo;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class TesteController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    private TextField textoMensagem;
+
+    public void abrirTelaHello() throws IOException {
+        /*var fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        Stage stage = new Stage();
+        stage.setTitle("Hello!");
+        stage.setScene(scene);
+        stage.show();*/
+
+        //Ler valor do textField
+        welcomeText.setText(textoMensagem.getText());
+    }
+}
